9d7ad1683666378091022460455a0ddff0429332
Adrian <adrian.lin@student.unsw.edu.au>
2015-02-27 21:31:17 +1100

    Add routes related to product controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,16 @@
 */
 
 Route::get('/', ['uses'=>'ProductController@index', 'as'=>'home']);
+Route::resource('product', 'ProductController', ['only'=>'show']);
 
 Route::get('auth/login', ['uses'=>'Auth\AuthController@getLogin', 'as'=>'auth.login']);
 Route::post('auth/login', ['uses'=>'Auth\AuthController@postLogin']);
 Route::get('auth/logout', ['uses'=>'Auth\AuthController@getLogout', 'as'=>'auth.logout']);
+
+Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function(){
+    Route::get('/', function(){
+        return Redirect::route('admin.product.index');
+    });
+
+    Route::resource('product', 'ProductController', ['except'=>'show']);
+});
