9c077bb49b5a2cdbb6e42b989f9f05a6c39198a7
Adrian <adrian.lin@student.unsw.edu.au>
2015-02-27 20:39:33 +1100

    Add products migration
    Schema for the products table.

---
+++ database/migrations/2015_02_27_092946_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('description');
+            $table->decimal('price', 9, 2);
+            $table->string('image', 100);
+            $table->char('stock_number', 10);
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+
+}
