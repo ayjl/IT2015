ddbab0eab2dd5c47ad842ba2c94a71cc662603bd
Adrian <adrian.lin@student.unsw.edu.au>
2015-02-28 11:15:34 +1100

    Add product creation functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,9 @@
 <?php namespace App\Http\Controllers;
 
 use App\Product;
+use \Validator;
+use \Input;
+use \Redirect;
 
 class ProductController extends Controller {
 
@@ -36,6 +39,46 @@ class ProductController extends Controller {
     public function create()
     {
         return view('product.create');
+    }
+
+    /**
+     * Store the product.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $rules = array(
+           'name'           => 'required|string|max:100'
+           ,'description'   => 'required|string'
+           ,'price'         => 'required|numeric'
+           ,'image'         => 'required|image|max:1024'
+           ,'stock_number'  => 'required|alpha_num|size:10'
+           ,'available'     => 'in:1'
+        );
+
+        $validator = Validator::make(Input::all(), $rules);
+
+        if($validator->passes())
+        {
+            $image = Input::file('image');
+            $fileLocation = public_path().'/images/';
+            $fileName = uniqid().'.'.$image->getClientOriginalExtension();
+            $image->move($fileLocation, $fileName);
+
+            $product = new Product;
+            $product->name          = Input::get('name');
+            $product->description   = Input::get('description');
+            $product->price         = Input::get('price');
+            $product->image         = $fileName;
+            $product->stock_number  = Input::get('stock_number');
+            $product->available     = Input::get('available', false);
+
+            $product->save();
+
+           return Redirect::route("admin.product.edit", [$product->id]);
+        }
+        return Redirect::route('admin.product.create')->withInput()->withErrors($validator);
     }
 
 }
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -2,15 +2,70 @@
 
 @section('content')
 <div class="container">
-    <h1>
-        Products
-        @if(strpos(Request::url(), 'admin') !== false)
-        <a href="{{URL::route('admin.product.create')}}" class="btn btn-primary">New Product</a>
-        @endif
-    </h1>
+    <h1>Create Product</h1>
 
-    @foreach($products as $product)
-        <p>{{$product->name}}</p>
-    @endforeach
+    @if(sizeof($errors->all()) != 0)
+       <div class="alert alert-danger">
+       @foreach($errors->all() as $error)
+          <p>{{ $error }}</p>
+       @endforeach
+       </div>
+    @endif
+
+    <form method="POST" action="{{URL::route('admin.product.store')}}" enctype="multipart/form-data">
+        <input type="hidden" name="_token" value="{{csrf_token()}}">
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="name" class="control-label required">Product name</label>
+                    <input id="name" name="name" type="text" class="form-control" required value="{{ old('name') }}">
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="stock_number" class="control-label required">Stock number</label>
+                    <input id="stock_number" name="stock_number" type="text" class="form-control" required value="{{ old('stock_number') }}">
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="description" class="control-label required">Description</label>
+            <textarea id="description" name="description" type="text" class="form-control" required>{{ old('description') }}</textarea>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="price" class="control-label required">Price</label>
+                    <div class="input-group">
+                        <div class="input-group-addon">$</div>
+                        <input id="price" name="price" type="number" step="0.01" class="form-control" required value="{{ old('price') }}">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div class="form-group">
+                    <label for="stock_number" class="control-label required">Availability</label>
+                    <div class="checkbox">
+                        <label>
+                            <input name="available" type="checkbox" value="1" {{old('available')? 'checked':''}}>Available
+                        </label>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="image" class="control-label required">Image</label>
+            <input id="image" name="image" type="file" class="form-control" required>
+        </div>
+
+        <input type="submit" value="Save" class="btn btn-primary">
+    </form>
 </div>
+
 @endsection
