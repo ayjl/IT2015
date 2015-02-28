c88c4b645e482c4289340c4b140c9b918e30efce
Adrian <adrian.lin@student.unsw.edu.au>
2015-02-28 11:14:54 +1100

    Change site name to 'Built'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <title>Built</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 
@@ -30,11 +30,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{URL::route('home')}}">Laravel</a>
+                <a class="navbar-brand" href="{{URL::route('home')}}">Built</a>
             </div>
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
+                    <li><a href="{{URL::route('admin.product.index')}}">Admin</a></li>
                 </ul>
 
                 <ul class="nav navbar-nav navbar-right">
