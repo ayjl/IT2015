6521267e89b639c7522e66615f973ebd66a920f3
Adrian <adrian.lin@student.unsw.edu.au>
2015-02-28 12:41:30 +1100

    Show product image in description and form buttons

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -67,12 +67,42 @@
         </div>
 
         <div class="form-group">
-            <label for="image" class="control-label required">Image</label>
-            <input id="image" name="image" type="file" class="form-control" required>
+            <label for="image" class="control-label {{isset($product->id)? '':'required'}}">Image</label>
+            <input id="image" name="image" type="file" class="form-control" {{isset($product->id)? '':'required'}}>
         </div>
 
-        <input type="submit" value="Save" class="btn btn-primary">
+        @if(isset($product->id))
+        <div class="product-image" style="background-image:url('{{URL::to($product->image)}}')"></div>
+        @endif
+
+        <div class="button-row">
+            <input type="submit" value="Save" class="btn btn-primary">
+            <a href="{{URL::route('admin.home')}}" class="btn btn-default">Back</a>
+        </div>
     </form>
+
+    @if(isset($product->id))
+    <div class="button-row">
+        <form>
+            <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <input type="hidden" name="_method" value="DELETE">
+
+            <div class="btn-group">
+                <span class="btn btn-danger no-btn-danger">Delete</span>
+
+                <span class="btn btn-danger dropdown-toggle no-disable" data-toggle="dropdown" aria-expanded="false">
+                    <span class="caret"></span>
+                </span>
+
+                <ul class="dropdown-menu" data-response-id="53">
+                    <li>
+                        <a href="">Delete</a>
+                    </li>
+                </ul>
+            </div>
+        </form> 
+    </div>
+    @endif
 </div>
 
 @endsection
